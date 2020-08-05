6b50ed667732f18e0e7c3bfd5f268f3d213ccb05
Stephen Miller <stephen.miller7@gmail.com>
2020-08-04 21:40:54 -0400

    successful getting back all interfaces from steam!

--- items.py
+++ items.py
@@ -1,5 +1,15 @@
 import os
+import json
+import requests
 
 print("Welcome to the item analysis program\n")
 print("Lets see the current items!")
 apikey = os.environ.get('dota2apikey')
+
+# See http://wiki.teamfortress.com/wiki/WebAPI for a list of available 
+# interfaces and resources(methods) available.
+
+request_url = "http://api.steampowered.com/ISteamWebAPIUtil/GetSupportedAPIList/v0001/?key=" + apikey
+
+allInterfaces = requests.get(request_url)
+print(allInterfaces.json())
